7b8cf73d3fc60ce2b215e69060c828c91951a7b6
Aidan Pendergast <append08@gmail.com>
2024-11-10 14:50:43 -0700

    added counter

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Counter from '../components/Counter';
 
 export default function Home() {
   return (
@@ -13,7 +14,9 @@ export default function Home() {
           Welcome to Aidan's <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-    
+
+        <Counter/>
+
 
       <style jsx>{`
         main {
